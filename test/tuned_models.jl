2324db4ef670ecdc88457ffa25500e34864a55ef
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-01-04 15:03:55 +1300

    rm commented-out test

--- test/tuned_models.jl
+++ test/tuned_models.jl
@@ -38,19 +38,6 @@ r = [m(K) for K in 2:13]
                TunedModel(model=first(r), tuning=Explicit(), range=r))
 end
 
-# @testset "duplicate models warning" begin
-#     s = [m(K) for K in 2:13]
-#     push!(s, m(13))
-#     tm = TunedModel(model=first(s), tuning=Explicit(),
-#                     range=s, resampling=CV(nfolds=2),
-#                     measures=[rms, l1])
-#     @test_logs((:info, r"Attempting"),
-#                (:warn, r"A model already"),
-#                fitresult, meta_state, report = fit(tm, 1, X, y))
-#     history, _, state = meta_state;
-#     @test length(history) == length(2:13) + 1
-# end
-
 results = [(evaluate(model, X, y,
                      resampling=CV(nfolds=2),
                      measure=rms,
@@ -161,6 +148,8 @@ end
                      @test all(history) do event
                          event.metadata == event.model.K
                      end
-end)
+                     end)
+
+
 
 true
