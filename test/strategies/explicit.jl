ae4b3f5cb4657bf6048d6b9fad6b43a7c2ad628f
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-07-27 09:10:51 +1200

    add a test that good, bad, and ugly are ordered as asserted in comment

--- test/strategies/explicit.jl
+++ test/strategies/explicit.jl
@@ -4,13 +4,8 @@ ugly = ConstantClassifier()
 
 r = [good, bad, ugly]
 
-rng=StableRNGs.StableRNG(123)
+rng = StableRNGs.StableRNG(123)
 X, y = make_blobs(rng=rng)
-
-@testset "constructor" begin
-true 
-end
-
 
 @testset_accelerated "integration" accel begin
 
@@ -20,17 +15,11 @@ end
         e = evaluate(model, X, y, resampling=resampling, measure=LogLoss())
         return e.measurement[1]
     end
-    # scores are ordered best to worst:
-    # 3-element Vector{Float64}:
-    #  2.2204460492503136e-16
-    #  0.005578588782855459
-    #  1.121552757665965
+    @test scores == sort(scores)
 
     # find the scores using `Explicit` tuning:
-    tmodel = TunedModel(model=first(r),
-                        range=r,
+    tmodel = TunedModel(models=r,
                         resampling=resampling,
-                        tuning=Explicit(),
                         measure=LogLoss(),
                         acceleration=accel)
     mach = machine(tmodel, X, y)
