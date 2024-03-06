70445e7e188052b499f9758a06d87092a38ec909
Anthony D. Blaom <anthony.blaom@gmail.com>
2024-03-07 08:06:10 +1300

    view -> id

--- test/tuned_models.jl
+++ test/tuned_models.jl
@@ -411,14 +411,14 @@ end
 thing = []
 struct EphemeralRegressor <: Deterministic end
 function MLJBase.fit(::EphemeralRegressor, verbosity, X, y)
-    # if I serialize/deserialized `thing` then `view` below changes:
-    view = objectid(thing)
-    fitresult = (thing, view, mean(y))
+    # if I serialize/deserialized `thing` then `id` below changes:
+    id = objectid(thing)
+    fitresult = (thing, id, mean(y))
     return fitresult, nothing, NamedTuple()
 end
 function MLJBase.predict(::EphemeralRegressor, fitresult, X)
-    thing, view, μ = fitresult
-    return view == objectid(thing) ? fill(μ, nrows(X)) :
+    thing, id, μ = fitresult
+    return id == objectid(thing) ? fill(μ, nrows(X)) :
         throw(ErrorException("dead fitresult"))
 end
 function MLJBase.save(::EphemeralRegressor, fitresult)
@@ -427,8 +427,8 @@ function MLJBase.save(::EphemeralRegressor, fitresult)
 end
 function MLJBase.restore(::EphemeralRegressor, serialized_fitresult)
     thing, μ = serialized_fitresult
-    view = objectid(thing)
-    return (thing, view, μ)
+    id = objectid(thing)
+    return (thing, id, μ)
 end
 
 @testset "save and restore" begin
